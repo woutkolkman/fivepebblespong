41c23479a90d9bea91de7038504781b8710d6c8e
woutkolkman <wout.kolkman1@hotmail.com>
2023-03-29 21:44:59 +0200

    images are displaying

--- FivePebblesPong/Games/Capture.cs
+++ FivePebblesPong/Games/Capture.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace FivePebblesPong
@@ -11,8 +12,16 @@ namespace FivePebblesPong
     public class Capture : FPGame
     {
         Process captureProcess;
-        Queue<byte[]> imgQueue = new Queue<byte[]>();
-        Mutex newFrameMtx = new Mutex(); //prevents queue from being used at the same time
+        public int frame = 0;
+
+        Queue<byte[]> imgLoad = new Queue<byte[]>();
+        Mutex imgLoadMtx = new Mutex(); //prevents queue from being used twice at the same time
+
+        Queue<ProjectedImage> imgLoiter = new Queue<ProjectedImage>();
+        public const int IMG_LOITER_COUNT = 3; //prevents flashing images
+
+        Queue<string> imgUnload = new Queue<string>();
+        public const int IMG_UNLOAD_AT_COUNT = 4; //delay atlas unload so game doesn't throw exceptions
 
 
         public Capture(OracleBehavior self) : base(self)
@@ -42,13 +51,35 @@ namespace FivePebblesPong
         public override void Destroy()
         {
             base.Destroy(); //empty
-            imgQueue.Clear();
+            imgLoad.Clear();
             bool hasExited = captureProcess == null || captureProcess.HasExited;
             captureProcess?.CloseMainWindow();
             captureProcess?.Close();
             captureProcess = null;
+
+            //TODO program keeps running if exiting RainWorld while Capture was active
+
             if (!hasExited)
-                FivePebblesPong.ME.Logger_p.LogInfo("Capture, Closed CaptureOBS");
+                FivePebblesPong.ME.Logger_p.LogInfo("Capture.Destroy, Closed CaptureOBS");
+
+            while (imgLoiter.Count > 0) {
+                ProjectedImage img = imgLoiter.Dequeue();
+                img.RemoveFromRoom();
+                foreach (string name in img.imageNames) {
+                    FivePebblesPong.ME.Logger_p.LogInfo("Capture.Destroy, RemoveFromRoom: \"" + name + "\"");
+                    imgUnload.Enqueue(name);
+                }
+            }
+
+            Task deload = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(1000);
+                while (imgUnload.Count > 0) {
+                    string name = imgUnload.Dequeue();
+                    FivePebblesPong.ME.Logger_p.LogInfo("Capture.Destroy task, Unload: \"" + name + "\"");
+                    Futile.atlasManager.ActuallyUnloadAtlasOrImage(name);
+                }
+            });
         }
 
 
@@ -56,29 +87,59 @@ namespace FivePebblesPong
         {
             base.Update(self);
 
-            if (imgQueue.Count <= 0)
+            if (imgLoiter.Count >= IMG_LOITER_COUNT) {
+                ProjectedImage img = imgLoiter.Dequeue();
+                self.oracle.room.RemoveObject(img);
+                foreach (string name in img.imageNames)
+                    imgUnload.Enqueue(name);
+            }
+
+            if (imgUnload.Count >= IMG_UNLOAD_AT_COUNT)
+                Futile.atlasManager.ActuallyUnloadAtlasOrImage(imgUnload.Dequeue());
+
+            if (imgLoad.Count <= 0)
                 return;
 
-            //get new frame and create Texture2D
-            if (!newFrameMtx.WaitOne(50)) {
+            //get new frame and save
+            if (!imgLoadMtx.WaitOne(50)) {
                 FivePebblesPong.ME.Logger_p.LogInfo("Capture.Update, Mutex timeout");
                 return;
             }
+
+            Texture2D newFrame = new Texture2D(0, 0);
             try {
-                byte[] imageBase64 = imgQueue.Dequeue();
-                Texture2D newFrame = new Texture2D(0, 0);
+                byte[] imageBase64 = imgLoad.Dequeue();
                 newFrame.LoadImage(imageBase64);
-                FivePebblesPong.ME.Logger_p.LogInfo("Capture.Update, newFrame: " + newFrame.width + "x" + newFrame.height);
             } catch (Exception ex) {
-                FivePebblesPong.ME.Logger_p.LogInfo("Capture.Update, Error parsing data: " + ex.ToString());
+                FivePebblesPong.ME.Logger_p.LogInfo("Capture.Update, Error storing data: " + ex.ToString());
             }
-            newFrameMtx.ReleaseMutex();
+            imgLoadMtx.ReleaseMutex();
+
+            if (newFrame?.width <= 0 || newFrame.height <= 0)
+                return;
+
+            string imgName = "FPP_Window_" + frame++;
+            //FivePebblesPong.ME.Logger_p.LogInfo("Capture.Update, Creating: \"" + imgName + "\"");
+
+            //load and display new frame
+            ProjectedImage temp;
+            if ((self is SLOracleBehavior && !ModManager.MSC) || self is MoreSlugcats.SSOracleRotBehavior) {
+                temp = new MoonProjectedImageFromMemory(new List<Texture2D> { newFrame }, new List<string> { imgName }, 0);
+            } else {
+                temp = new ProjectedImageFromMemory(new List<Texture2D> { newFrame }, new List<string> { imgName }, 0);
+            }
+            temp.pos = new Vector2(midX, midY);
+            imgLoiter.Enqueue(temp);
+            self.oracle.myScreen.room.AddObject(temp);
         }
 
 
         public override void Draw(Vector2 offset)
         {
             //update image positions
+            /*if (window == null)
+                return;
+            window.pos = new Vector2(midX, midY);*/
         }
 
 
@@ -95,7 +156,7 @@ namespace FivePebblesPong
                 return;
             }
 
-            if (imgQueue.Count > 5) {
+            if (imgLoad.Count > 5) {
                 FivePebblesPong.ME.Logger_p.LogInfo("Capture.DataReceivedEvent, Byte[] queue too large, dropping frame... [" + droppedFrames++ + "]");
                 return;
             }
@@ -110,12 +171,12 @@ namespace FivePebblesPong
             }
 
             //the byte array MUST be queued, calling the Texture2D ctor here crashes the game apparently
-            if (!newFrameMtx.WaitOne(50)) {
+            if (!imgLoadMtx.WaitOne(50)) {
                 FivePebblesPong.ME.Logger_p.LogInfo("Capture.DataReceivedEvent, Mutex timeout");
                 return;
             }
-            imgQueue.Enqueue(imageBase64);
-            newFrameMtx.ReleaseMutex();
+            imgLoad.Enqueue(imageBase64);
+            imgLoadMtx.ReleaseMutex();
         }
     }
 }
